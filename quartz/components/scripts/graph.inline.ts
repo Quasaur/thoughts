1249f551af19845d1fb046d62eed6e82747b7e95
Calvin L. Mitchell <devcalvinlm@gmail.com>
2024-07-14 18:32:22 -0400

    14th attempt to write NodeData to file using new function debugWrite.

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -2,7 +2,7 @@ import type { ContentDetails, ContentIndex } from "../../plugins/emitters/conten
 import * as d3 from "d3"
 import { registerEscapeHandler, removeAllChildren } from "./util"
 import { FullSlug, SimpleSlug, getFullSlug, resolveRelative, simplifySlug } from "../../util/path"
-import fs from "platform 'node:fs'";
+var fs: { writeFile: (arg0: string, arg1: any) => any; } any = require('fs');
 
 
 type NodeData = {
